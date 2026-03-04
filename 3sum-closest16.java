ac7f1e52c6e1960ecce030c099ce7af4c4806406
VISHAL <168129896+Vishal8500@users.noreply.github.com>
2026-03-04 20:27:00 +0530

    Implement threeSumClosest function to find closest sum

---
+++ 3sum-closest16.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        
+        int res=nums[0]+nums[1]+nums[2];
+        int mindiff=Integer.MAX_VALUE;
+
+        for(int i=0;i<nums.length-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+
+            int left=i+1;
+            int right=nums.length-1;
+
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+
+                if(sum==target){
+                    return sum;
+                }
+                else if(sum<target){
+                    left++;
+                }
+                else{ 
+                    right--;
+                }
+                
+                int sumdiff=Math.abs(sum-target);
+
+                if(sumdiff<mindiff){
+                    res=sum;
+                    mindiff=sumdiff;
+                }
+            }
+        }
+        return res;
+    }
+}
